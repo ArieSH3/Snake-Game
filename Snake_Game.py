65cd6a83af97099232ab78b14c4b9bb787a8adb7
ArieSH3 <StepDubUp@gmail.com>
2021-06-15 15:24:26 +0200

    On the right track but things need to be fixed and functions added

--- Snake_Game.py
+++ Snake_Game.py
@@ -20,19 +20,20 @@
 
 import pygame
 import sys
+import random
 
 
 # Define COLOURS
 WHITE = (255,255,255)
 BLACK = (0  ,0  ,0  )
-GREY  = (53 ,50 ,50 )
+GREY  = (15 ,15 ,15 )
 GREEN = (0  ,255,0  )
 RED   = (255,0  ,0  )
 BLUE  = (0  ,0  ,255)
 
 # ____________________SET PARAMETERS____________________
 # Set FPS
-FPS = 60
+FPS = 10
 # Set GRID BLOCK size
 BLOCK_SIZE = 50  # 60 #_________Size of the individual square in grid
 # Set SNAKE colour
@@ -76,7 +77,7 @@ class Snake:
 		grid_colour_1=GRID_COL_1,
 		grid_colour_2=GRID_COL_2,
 		food_colour=FOOD_COL,
-		block_size=BLOCK_SIZE,
+		block_size=BLOCK_SIZE
 		#game_display = (480, 480)
 	):
 
@@ -86,17 +87,38 @@ class Snake:
 		self.controls_type = controls_type
 		self.fps = fps
 		self.snake_colour = snake_colour
-		self.grid_colour = grid_colour
+		self.grid_colour_1 = grid_colour_1
+		self.grid_colour_2 = grid_colour_2
 		self.food_colour = food_colour
 		self.block_size = block_size
+
+		self.positions = [((self.grid_size/2),(self.grid_size/2))]
+		self.snake_length = 2
+
+		self.right = 1
+		self.left  = 2
+		self.up	   = 3
+		self.down  = 4
+			# Initial snake direction
+		self.direction = random.choice([self.right, self.left, self.up, self.down])
 			# Size of the game window
 		#self.screen = pygame.display.set_mode()
 			# Size of the playable area
 		#self.game_display = game_display
 
 
-	def move_head(self):
-		pass
+	def move_head(self, direction):
+		x = self.positions[0][0]
+		y = self.positions[0][1]
+
+		if self.direction == self.right:
+			x += self.block_size
+		elif self.direction == self.left:
+			x -= self.block_size
+		elif self.direction == self.up:
+			y -= self.block_size
+		elif self.direction == self.down:
+			y += self.block_size
 
 	def move_snake(self):
 		pass
@@ -105,19 +127,47 @@ class Snake:
 	def draw_grid(self):
 		for y in range(self.grid_size):
 			for x in range(self.grid_size):
-				rect = pygame.Rect((x*self.block_size, y*self.block_size), (block_size, block_size))
-				if (i+j)%2 == 0:
+				rect = pygame.Rect((x*self.block_size, y*self.block_size), (self.block_size, self.block_size))
+					# This if/else used to create a checkered grid
+				if (x+y)%2 == 0:
 					pygame.draw.rect(screen, self.grid_colour_1, rect)
 				else:
 					pygame.draw.rect(screen, self.grid_colour_2, rect)
 
-	def draw_snake():
-		pass
+	def draw_snake(self):
+		for pos in self.positions:
+			rect = pygame.Rect((pos[0]*self.block_size, pos[1]*self.block_size), (self.block_size, self.block_size))
+			pygame.draw.rect(screen, self.snake_colour, rect)
+
+	def handle_keys(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT and self.direction != self.left:
+					self.direction = self.right
+				if event.key == pygame.K_LEFT and self.direction != self.right:
+					self.direction = self.left
+				if event.key == pygame.K_UP and self.direction != self.down:
+					self.direction = self.up
+				if event.key == pygame.K_DOWN and self.direction != self.up:
+					self.direction = self.down
+
 
 
 
 def main():
+	snake = Snake()
+	
 	while True:
-		Snake().draw_grid()
+		snake.draw_grid()
+		snake.draw_snake()
+		snake.handle_keys()
+		snake.move_head()
 
 		pygame.display.update()
+
+
+main()
